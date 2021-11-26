d611e76cacc329923ae0768545eaa68f9ce86727
CarliniBreno <marcondesbreno20@gmail.com>
2021-11-26 20:49:41 -0300

    Margin correction (rs)

--- parte_js.js
+++ parte_js.js
@@ -1,33 +1,18 @@
-function alerta(){
+function verificarLogin(){
     var login = document.getElementById("login").value;
     var password = document.getElementById("password").value;
-    
-    if (login == "flavio@basesocial.org") {
-        alert ("Login Succesfully!")
-    }
-    else {
-        alert ("Login failed.")
-    }
-    if (password == "macacoflavio") {
-        alert ("Login Succesfully!")
-    }
-    else {
-        alert ("Login failed.")
-    }
-}
 
-function verificarLogin(){
-    var emailCerto = document.getElementById("email").value;
-    var senhaCerto = document.getElementById("senha").value;
-
-    if(emailCerto == "caioba123@gmail.com"){
-        if(senhaCerto == "123"){
-            alert("Usuário logado com sucesso!")
+    if(login == "flavio@basesocial.org"){
+        if(password == "123"){
+            alert("Login Succesfully!")
         } else{
-            alert("Algo deu errado, verefique o email e a senha")
+            alert("Login failed")
         }
 
     }else{
-        alert("Algo deu errado, verefique o email e a senha")
+        alert("Login failed")
+    }
+    if(login == ""){
+        alert("Insira seus dados!")
     }
 }
